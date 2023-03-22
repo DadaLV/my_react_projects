61b2d9daa5e1d5c88e8dac0cd60a60926ed6d91e
DadaLV <vynohradova.l.a@gmail.com>
2023-03-23 00:38:35 +0100

    modified

--- 21-jsonplaceholder-posts/src/components/Posts.js
+++ 21-jsonplaceholder-posts/src/components/Posts.js
@@ -1,22 +1,38 @@
 import { useState, useEffect } from "react";
 import Post from "./Post";
 
+const API_URL = "https://jsonplaceholder.typicode.com/posts"
+
 function Posts() {
   const [posts, setPosts] = useState([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
+    fetch(API_URL)
       .then((res) => res.json())
       .then((posts) => {
-        console.log(posts);
+        // console.log(posts);
         setPosts(posts);
       })
-      .catch((error) => console.log(error.message));
+      .catch((error) => setError(error.message));
+    // .finally(() => setIsLoading(false))
   }, []);
 
+  if (error) {
+    return <h1>Error: {error}</h1>;
+  }
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <h1>Posts</h1>
+      <hr />
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        posts.map((post) => <Post key={post.id} {...post} />)
+      )}
+    </>
   );
 }
 
