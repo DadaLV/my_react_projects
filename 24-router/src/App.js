8de91b66f52a774d71c97acde12bda7ef65f6dc3
DadaLV <vynohradova.l.a@gmail.com>
2023-04-03 01:05:27 +0200

    add routes

--- 24-router/src/App.js
+++ 24-router/src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="about" element={<h1>About</h1>} />
+          <Route path="contacts" element={<h1>Contacts</h1>} />
+          <Route path="*" element={<h1>Not Found</h1>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
