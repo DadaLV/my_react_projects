4695a11d77224109c1e1afbae3715b7214f65b5a
DadaLV <vynohradova.l.a@gmail.com>
2023-04-04 22:16:26 +0200

    add data courses

---
+++ 24-router/src/data/courses.js
@@ -0,0 +1,24 @@
+const courses = [
+  {
+    title: "Complete React Guide",
+    slug: "react",
+    id: 258,
+  },
+  {
+    title: "Complete Ruby Guide",
+    slug: "ruby",
+    id: 278,
+  },
+  {
+    title: "Complete JavrScript Guide",
+    slug: "javascript",
+    id: 758,
+  },
+  {
+    title: "Complete Node.js Guide",
+    slug: "node",
+    id: 777,
+  },
+];
+
+export default courses;
