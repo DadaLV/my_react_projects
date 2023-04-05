6243592b6dbf74e5046fc5eee757483aec7f3db5
DadaLV <vynohradova.l.a@gmail.com>
2023-04-05 23:42:31 +0200

    add single course

--- 24-router/src/SingleCourse.jsx
+++ 24-router/src/SingleCourse.jsx
@@ -1,6 +1,17 @@
-import {courses} from "../src/data/courses"
+import { Link, useParams } from "react-router-dom";
+import courses from "../src/data/courses";
+
 function SingleCourse() {
-  return <h1>SingleCourseInfo</h1>;
+  const params = useParams();
+  const course = courses.find((course) => course.slug === params.slug);
+  return (
+    <>
+      <h1>{course.title}</h1>
+      <h2>{course.slug}</h2>
+      <h3>{course.id}</h3>
+      <Link to="/courses">All courses</Link>
+    </>
+  );
 }
 
 export default SingleCourse;
