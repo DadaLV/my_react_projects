1ebf354318338c3d890b76826891bff5343bfe2e
DadaLV <vynohradova.l.a@gmail.com>
2023-04-06 23:40:28 +0200

    add useEffect and navigate

--- 24-router/src/SingleCourse.jsx
+++ 24-router/src/SingleCourse.jsx
@@ -1,26 +1,36 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import courses from "../src/data/courses";
 import NotFound from "./NotFound";
+import { useEffect } from "react";
 
 function SingleCourse() {
   const params = useParams();
+  const navigate = useNavigate()
   const course = courses.find((course) => course.slug === params.slug);
-  if (course === undefined) {
-    return (
-      <>
-        <NotFound />
-        <Link to=".." relative="path">
-          All courses
-        </Link>
-      </>
-    );
+  // Simply shoe NotFound component
+  // if (course === undefined) {
+  //   return (
+  //     <>
+  //       <NotFound />
+  //       <Link to=".." relative="path">
+  //         All courses
+  //       </Link>
+  //     </>
+  //   );
+  // }
+
+  
+  useEffect(() => {
+if (!course) {
+    navigate("..", {relative: "path"})
   }
+  }, [course])
 
   return (
     <>
-      <h1>{course.title}</h1>
-      <h2>{course.slug}</h2>
-      <h3>{course.id}</h3>
+      <h1>{course?.title}</h1>
+      <h2>{course?.slug}</h2>
+      <h3>{course?.id}</h3>
       <Link to=".." relative="path">
         All courses
       </Link>
