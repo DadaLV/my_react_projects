196a4200b25682b0734c7e5d6af998567170befd
DadaLV <vynohradova.l.a@gmail.com>
2023-04-08 00:12:01 +0200

    add redirect

--- 24-router/src/Courses.jsx
+++ 24-router/src/Courses.jsx
@@ -1,10 +1,15 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import queryString from "query-string";
 import courses from "../src/data/courses";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const SORT_KEYS = ["title", "id", "slug"]
 
 function sortCourses(courses, key) {
   const sortedCourses = [...courses];
+  if (!key || !SORT_KEYS.includes(key)) {
+    return sortedCourses
+  }
   sortedCourses.sort((a, b) => a[key] > b[key] ? 1 : -1);
   return sortedCourses;
 }
@@ -14,9 +19,16 @@ function Courses() {
   const query = queryString.parse(location.search);
   const [sortKey, setSortKey] = useState(query.sort);  
   const [sortedCourses, setSortedCourses] = useState(sortCourses(courses, sortKey));
+  const navigate = useNavigate()
+  useEffect(() => {
+    if (!SORT_KEYS.includes(sortKey)) {
+      navigate(".")
+      setSortKey()
+    }
+  }, [sortKey, navigate])
   return (
     <>
-      <h1>Courses</h1>
+      <h1>{ sortKey ? `Courses sorted by ${sortKey}` : "Courses"}</h1>
       {sortedCourses.map((course) => (
         <div key={course.id}>
           <Link to={course.slug} className="courseLink">
