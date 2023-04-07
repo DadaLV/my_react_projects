6c0f00881f3053fc8f7b157c7ee26c53bcf626bb
DadaLV <vynohradova.l.a@gmail.com>
2023-04-08 01:03:37 +0200

    add components user and userInfo

---
+++ 27-context-api/src/App.js
@@ -0,0 +1,14 @@
+import {useState } from "react"
+import './App.css';
+import User from "./components/User";
+
+function App() {
+  const [user, setUser] = useState("Liuba")
+  return (
+    <div className="App">
+      <User user={ user} />
+    </div>
+  );
+}
+
+export default App;
